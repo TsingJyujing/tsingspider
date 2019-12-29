158f6190a1b737df4a8cbc92e3358dc182e8d0d9
Yuan Yifan <yifan.yuan@linecorp.com>
2019-12-29 23:00:22 +0900

    Update HTTP client, support change the settings dynamically

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ from setuptools import find_packages
 
 setup(
     name="TsingSpider",
-    version=1.1,
+    version=1.2,
     install_requires=[
         "requests",
         "BeautifulSoup4",
         "lxml"
     ],
-    packages=find_packages()
+    packages=find_packages(
+        exclude=(
+            "tsing_spider.test"
+        )
+    )
 )
